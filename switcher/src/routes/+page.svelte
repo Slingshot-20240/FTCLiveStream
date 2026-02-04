2e61084e8964a62fb9358bff33fda8b146a2c25d
Jining Liu <hello@jining.dev>
2026-02-04 00:01:49 -0600

    cam switcher path setup

---
+++ switcher/src/routes/+page.svelte
@@ -0,0 +1,13 @@
+<script lang="ts">
+    import { onMount } from 'svelte';
+
+    onMount(() => {
+        // you'll need this; html elements cannot be accessed in svelte script block until the page has been "mounted"
+    });
+</script>
+
+<h1>hi</h1>
+
+<style>
+
+</style>
